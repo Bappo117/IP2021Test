c3892659f07e28fc229dc5a87adf9d07f35b79ba
Bappo117 <74079889+Bappo117@users.noreply.github.com>
2021-03-14 17:24:25 -0400

    Add files via upload

---
+++ FallProject.java
@@ -0,0 +1,29 @@
+package FallProject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class FallProject extends Application{
+     public static void main(String[] args) {launch(args);}
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
+       Scene scene = new Scene(root);
+       scene.getStylesheets().add(getClass().getResource("view/style.css").toExternalForm());
+       
+       stage.setTitle("Fractals");
+      stage.setScene(scene);
+      
+      System.out.println(root.getChildrenUnmodifiable().get(1));
+       
+       stage.show();
+
+
+
+    } 
+}
