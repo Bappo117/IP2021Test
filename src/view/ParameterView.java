e3614963a68c03a0e8a8e9497253fddbb1a9a678
Greatone3145 <48657408+Greatone3145@users.noreply.github.com>
2021-03-21 21:20:55 -0400

    Add files via upload

--- src/view/ParameterView.java
+++ src/view/ParameterView.java
@@ -1,17 +1,23 @@
-package src.view;
+package view;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class ParameterView extends Pane {
     RadioButton[] rbList = new RadioButton[6];
+    private final ToggleGroup group = new ToggleGroup();
 
-    public ParameterView(){
+    public ParameterView() throws FileNotFoundException {
         super();
 
         //Title
@@ -20,14 +26,64 @@ public class ParameterView extends Pane {
         title.relocate(80, -3);
 
         //Radio Buttons
-        ToggleGroup tg = new ToggleGroup();
+        ToggleGroup tg = group;
         for(int i = 1; i < 7; i++){
-            rbList[i-1] = new RadioButton("Radio Button " + i);
+            rbList[i-1] = new RadioButton(/*"Radio Button " + i*/);
             rbList[i-1].setToggleGroup(tg);
         }
 
+        //Add Images
+        //House
+        Image house = new Image(new FileInputStream("C:\\Users\\aless\\IdeaProjects\\IP_Project_2021\\images\\house.png"));
+        ImageView ivHouse = new ImageView(house);
+        ivHouse.setFitHeight(30);
+        ivHouse.setFitWidth(30);
+        this.getChildren().add(ivHouse);
+        ivHouse.relocate(45, 40);
+
+        //Building
+        Image building = new Image(new FileInputStream("C:\\Users\\aless\\IdeaProjects\\IP_Project_2021\\images\\building.png"));
+        ImageView ivBuilding = new ImageView(building);
+        ivBuilding.setFitHeight(30);
+        ivBuilding.setFitWidth(30);
+        this.getChildren().add(ivBuilding);
+        ivBuilding.relocate(45, 80);
+
+
+        //Water Dam
+        Image waterdam = new Image(new FileInputStream("C:\\Users\\aless\\IdeaProjects\\IP_Project_2021\\images\\waterdam.png"));
+        ImageView ivWaterDam = new ImageView(waterdam);
+        ivWaterDam.setFitHeight(35);
+        ivWaterDam.setFitWidth(35);
+        this.getChildren().add(ivWaterDam);
+        ivWaterDam.relocate(160, 40);
+
+        //Wind Turbine
+        Image windturbine = new Image(new FileInputStream("C:\\Users\\aless\\IdeaProjects\\IP_Project_2021\\images\\windturbine.png"));
+        ImageView ivWindTurbine = new ImageView(windturbine);
+        ivWindTurbine.setFitHeight(30);
+        ivWindTurbine.setFitWidth(30);
+        this.getChildren().add(ivWindTurbine);
+        ivWindTurbine.relocate(160, 80);
+
+        //Solar Panel
+        Image solarpanel = new Image(new FileInputStream("C:\\Users\\aless\\IdeaProjects\\IP_Project_2021\\images\\solarpanel.png"));
+        ImageView ivSolarPanel = new ImageView(solarpanel);
+        ivSolarPanel.setFitHeight(30);
+        ivSolarPanel.setFitWidth(30);
+        this.getChildren().add(ivSolarPanel);
+        ivSolarPanel.relocate(160, 120);
+
+        //Nuclear Power Plant
+        Image npp = new Image(new FileInputStream("C:\\Users\\aless\\IdeaProjects\\IP_Project_2021\\images\\npp.png"));
+        ImageView ivNpp = new ImageView(npp);
+        ivNpp.setFitHeight(30);
+        ivNpp.setFitWidth(30);
+        this.getChildren().add(ivNpp);
+        ivNpp.relocate(160, 160);
+
         //First TextField
-        Text text1 = new Text("First Text");
+        Text text1 = new Text("Number of houses/buildings");
         text1.relocate(25, 255);
 
         TextField firstTextField = new TextField();
@@ -36,10 +92,10 @@ public class ParameterView extends Pane {
         firstTextField.setDisable(false); //Used to disable a text field
         firstTextField.setMaxWidth(100);
 
-        firstTextField.relocate(100, 250);
+        firstTextField.relocate(190, 250);
 
         //Second TextField
-        Text text2 = new Text("Second Text");
+        Text text2 = new Text("Consumption per week (hrs)");
         text2.relocate(25, 305);
 
         TextField secondTextField = new TextField();
@@ -48,10 +104,10 @@ public class ParameterView extends Pane {
         secondTextField.setDisable(false); //Used to disable a text field
         secondTextField.setMaxWidth(100);
 
-        secondTextField.relocate(100, 300);
+        secondTextField.relocate(190, 300);
 
         //Third TextField
-        Text text3 = new Text("Third Text");
+        Text text3 = new Text("Number of suppliers");
         text3.relocate(25, 355);
 
         TextField thirdTextField = new TextField();
@@ -60,32 +116,58 @@ public class ParameterView extends Pane {
         thirdTextField.setDisable(false); //Used to disable a text field
         thirdTextField.setMaxWidth(100);
 
-        thirdTextField.relocate(100, 350);
+        thirdTextField.relocate(190, 350);
 
         //Generate Button
         Button generateButton = new Button("Generate");
         generateButton.relocate(125, 400);
 
         this.getChildren().add(rbList[0]);
-        rbList[0].relocate(15, 40);
+        rbList[0].relocate(15, 45);
 
         this.getChildren().add(rbList[1]);
-        rbList[1].relocate(15, 80);
+        rbList[1].relocate(15, 85);
 
         this.getChildren().add(rbList[2]);
-        rbList[2].relocate(130, 40);
+        rbList[2].relocate(130, 45);
 
         this.getChildren().add(rbList[3]);
-        rbList[3].relocate(130, 80);
+        rbList[3].relocate(130, 85);
 
         this.getChildren().add(rbList[4]);
-        rbList[4].relocate(130, 120);
+        rbList[4].relocate(130, 125);
 
         this.getChildren().add(rbList[5]);
-        rbList[5].relocate(130, 160);
+        rbList[5].relocate(130, 165);
 
         this.getChildren().addAll(title, text1, text2, text3, firstTextField, secondTextField, thirdTextField, generateButton);
+
+        tg.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                if(rbList[0].isSelected() || rbList[1].isSelected()) {
+                    firstTextField.setDisable(false);
+                    secondTextField.setDisable(false);
+                    thirdTextField.setDisable(true);
+                }else if(rbList[2].isSelected() || rbList[3].isSelected() || rbList[4].isSelected() || rbList[5].isSelected()){
+                    firstTextField.setDisable(true);
+                    secondTextField.setDisable(true);
+                    thirdTextField.setDisable(false);
+
+                }else{
+                    firstTextField.setDisable(false);
+                    secondTextField.setDisable(false);
+                    thirdTextField.setDisable(false);
+                }
+            }
+        });
     }
 
+    public RadioButton[] getRbList(){
+        return rbList;
+    }
 
+    public ToggleGroup getToggleGroup(){
+        return group;
+    }
 }
